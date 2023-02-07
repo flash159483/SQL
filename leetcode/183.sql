40614ca0ad74920448c0fcb617ef9327c2453fcc
flash159483 <seungwonjung0313@gmail.com>
2023-02-07 22:22:58 +0900

    23/02/07

---
+++ leetcode/183.sql
@@ -0,0 +1,36 @@
+/*
+183. Customers Who Never Order
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+*/
+SELECT name as customers
+FROM Customers
+WHERE id not in (SELECT customerId from Orders)
